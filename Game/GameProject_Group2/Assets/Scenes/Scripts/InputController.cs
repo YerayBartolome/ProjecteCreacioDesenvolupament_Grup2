028007cd3d7362a77388f2c65d2823e9ad02b558
Álvaro <adepablos@edu.tecnocampus.com>
2020-04-15 22:06:17 +0200

    Input Controller y Movement
    Scripts Basicos

---
+++ Game/GameProject_Group2/Assets/Scenes/Scripts/InputController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float horizontalAxis, verticalAxis;
+    
+    void Start()
+    {
+        horizontalAxis = 0f;
+        verticalAxis = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+    }
+    public float HorizontalAxis
+    {
+        get{ return horizontalAxis;}
+    }
+    public float VerticalAxis
+    {
+        get { return verticalAxis; }
+    }
+
+
+
+}
