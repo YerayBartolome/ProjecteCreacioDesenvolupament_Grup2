f7f910d8775a1fb3701f54495e5542bb64c4b4b6
Roger <roger7a7@gmail.com>
2020-06-02 19:46:12 +0200

    workInPogres
    cameraGood

--- Game/GameProject_Group2/GameProject_Group2/Assets/Scripts/Camera Scripts/CameraController.cs
+++ Game/GameProject_Group2/GameProject_Group2/Assets/Scripts/Camera Scripts/CameraController.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
 
     Vector2 velocity;
-    [SerializeField]
-    float smoothTimeX = 0;
-    [SerializeField]
-    float smoothTimeY = 0;
-    [SerializeField]
-    GameObject spaceship;
+    [SerializeField] float smoothTimeX = 0;
+    [SerializeField] float smoothTimeY = 0, distance = 10;
+    [SerializeField] GameObject spaceship; 
+    Vector2 targetPosition;
+
 
     Plane m_SpaceshipPlane;
     Camera m_Caamera;
@@ -29,17 +29,28 @@ public class CameraController : MonoBehaviour
             //Input.moou
             Ray l_Ray=m_Caamera.ScreenPointToRay(Input.mousePosition);
             float l_Distance;
+            
             if (m_SpaceshipPlane.Raycast(l_Ray, out l_Distance))
             {
-                Vector2 l_Position = l_Ray.GetPoint(l_Distance);
-                m_DebugObject.transform.position = l_Position;
+                Vector2 l_Position = l_Ray.GetPoint(l_Distance);           
+                 m_DebugObject.transform.position = l_Position;                         
             }
+
+            if (Vector2.Distance((Vector2)m_DebugObject.transform.position, (Vector2)spaceship.transform.position) <= distance)
+            {
+                targetPosition = (new Vector2(m_DebugObject.transform.position.x, m_DebugObject.transform.position.y) -
+                                new Vector2(spaceship.transform.position.x, spaceship.transform.position.y))/2;
+            }
+
+            
+            Debug.Log(targetPosition);
+                       
             float posX, posY;
-            posX = Mathf.SmoothDamp(transform.position.x, spaceship.transform.position.x, ref velocity.x, smoothTimeX);
-            posY = Mathf.SmoothDamp(transform.position.y, spaceship.transform.position.y, ref velocity.y, smoothTimeY);
+            posX = Mathf.SmoothDamp(transform.position.x, targetPosition.x + spaceship.transform.position.x, ref velocity.x, smoothTimeX);
+            posY = Mathf.SmoothDamp(transform.position.y, targetPosition.y + spaceship.transform.position.y, ref velocity.y, smoothTimeY);
+
             
             transform.position = new Vector3(posX, posY, transform.position.z);
         }
     }
-
 }
