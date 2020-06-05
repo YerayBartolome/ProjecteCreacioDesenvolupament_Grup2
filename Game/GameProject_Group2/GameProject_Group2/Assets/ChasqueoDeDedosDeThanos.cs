5179852da1875dcab724dd4c731b881836067251
Álvaro <adepablos@edu.tecnocampus.com>
2020-06-05 15:56:34 +0200

    Final juego

---
+++ Game/GameProject_Group2/GameProject_Group2/Assets/ChasqueoDeDedosDeThanos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChasqueoDeDedosDeThanos : MonoBehaviour
+{
+    public float  secondsbeforeEnd = 5;
+    float timeToEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToEnd = Time.time+secondsbeforeEnd;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= timeToEnd){
+            SceneManager.LoadScene("Main Menu");
+        }
+
+    }
+}
