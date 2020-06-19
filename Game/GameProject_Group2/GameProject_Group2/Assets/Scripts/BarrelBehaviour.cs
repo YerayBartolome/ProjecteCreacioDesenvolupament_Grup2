844c071e57ee24592ce028cc118706dae2db10d8
Álvaro <adepablos@edu.tecnocampus.com>
2020-06-19 04:26:31 +0200

    barrel scripts

--- Game/GameProject_Group2/GameProject_Group2/Assets/Scripts/BarrelBehaviour.cs
+++ Game/GameProject_Group2/GameProject_Group2/Assets/Scripts/BarrelBehaviour.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class BarrelBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] drop;
+    public GameObject explotionObj;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnDestroy()
+    {   
+        System.Random rnd = new System.Random();
+        GameObject obj1 = Instantiate(explotionObj);
+
+        int index = rnd.Next(drop.Length);
+
+        GameObject obj2 = Instantiate(drop[index]);
     }
 }
---
+++ Game/GameProject_Group2/GameProject_Group2/Assets/Scripts/explotion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explotion : MonoBehaviour
+{
+    public float minForce = 0;
+    public float maxForce = 5;
+    public float explosionRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform t in transform)
+        {
+            var rb = t.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(Random.Range(minForce,maxForce), transform.position, explosionRadius);
+            }
+        }
+    }
+
+   
+}
