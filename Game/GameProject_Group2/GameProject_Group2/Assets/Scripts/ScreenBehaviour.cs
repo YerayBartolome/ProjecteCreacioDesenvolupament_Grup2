e9bbf2a9a1706ecb7ec0dc8cb5ef1afbb897e565
Álvaro <adepablos@edu.tecnocampus.com>
2020-06-19 02:50:44 +0200

    screenBehaviour

---
+++ Game/GameProject_Group2/GameProject_Group2/Assets/Scripts/ScreenBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBehaviour : MonoBehaviour
+{ 
+    [SerializeField] private GameObject interactMessage;
+    [SerializeField] private GameObject textMessage;
+
+    private float timeUntilSwap = 0f;
+    private bool showTextMessage = false;
+
+    private InputController input;
+   
+
+    private GameObject button;
+    private void Awake()
+    {
+        input = GetComponent<InputController>();
+        button = GetComponent<GameObject>();
+      
+        
+        interactMessage.SetActive(false);
+        textMessage.SetActive(false);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (showTextMessage) textMessage.SetActive(true);
+            else interactMessage.SetActive(true);
+            if (input.Interact && Time.time>timeUntilSwap) Swap();   
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactMessage.SetActive(false);
+            textMessage.SetActive(false);
+        }
+    }
+
+    private void Swap()
+    {
+        showTextMessage = !showTextMessage;
+        interactMessage.SetActive(false);
+        textMessage.SetActive(false);
+        timeUntilSwap = Time.time + 0.5f;
+        
+    }
+}
